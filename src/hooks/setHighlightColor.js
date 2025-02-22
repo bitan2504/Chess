b19040dcd2eadbbd719b5fa706f6aeff862efc45
Sayed036 <hossainsayed036@gmail.com>
2025-02-22 21:49:01 +0530

    added transitions and colour in selected box component

--- src/hooks/setHighlightColor.js
+++ src/hooks/setHighlightColor.js
@@ -3,13 +3,19 @@ import getBgColor from "./getBgColor";
 function setHighlightColor(board = [], highlight = []) {
     for (let i=0; i<8; i++) {
         for (let j=0; j<8; j++) {
+            const sq = document.getElementById(String(i) + String(j));
             if (highlight.some(e => e[0] === i && e[1] === j)) {
-                const sq = document.getElementById(String(i) + String(j));
-                if (board[i][j]) sq.style.backgroundColor = 'red';
-                else sq.style.backgroundColor = 'blue';
+                if (board[i][j]) {
+                    sq.style.backgroundColor = 'red';
+                    sq.style.transition = "background-color 0.3s ease";
+                }
+                else {
+                    sq.style.backgroundColor =' #71dfe7';
+                    sq.style.transition = "background-color 0.3s ease";
+                }
             } else {
-                const sq = document.getElementById(String(i) + String(j));
                 sq.style.backgroundColor = getBgColor(i, j);
+                sq.style.transition = "background-color 0.3s ease";
             }
         }
     }
